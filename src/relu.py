fcc45efdc6b0d1dfa9a526b47a6d3599c3ffe826
Snupiesp <2230129@upv.edu.mx>
2025-03-01 15:28:50 -0600

    Cambio 1

---
+++ src/relu.py
@@ -0,0 +1,7 @@
+def relu(x, derivate=False):
+    if derivate:
+        x[x<=0]=0
+        x[x>0]=1
+        return x
+    else:
+        return np.maximum(0,x)
---
+++ src/sigmoid.py
@@ -0,0 +1,5 @@
+def sigmoid(x, derivate=False):
+    if derivate:
+        return np.exp(-x)/(np.exp(-x)+1)**2
+    else:
+        return 1/(1+np.exp(-x))
